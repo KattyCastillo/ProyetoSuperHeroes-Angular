04f1f8e96c7df232676802d7dabbc31cbc94010a
Katty Stephany Castillo Galindo <kscastillo@grupofarsiman.com>
2022-02-08 15:07:27 -0600

    Uso del decorador Output para emitir eventos desde el componente hijo y que el padre lo resulva, al ser uso de eventos tambien se hace uso de EventEmitter

--- src/app/components/heroes-tarjetas/heroes-tarjetas.component.ts
+++ src/app/components/heroes-tarjetas/heroes-tarjetas.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input , Output, EventEmitter } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,15 +8,20 @@ import { Router } from '@angular/router';
 })
 export class HeroesTarjetasComponent implements OnInit {
 
-  constructor(private router: Router) { }
-
   @Input() heroeTarjeta: any = {};
   @Input() indexTarjeta!: number;
+
+  @Output() solicitarHeroe: EventEmitter<number>;
+
+  constructor(private router: Router) {
+    this.solicitarHeroe = new EventEmitter();
+   }
 
   ngOnInit(): void {
   }
 
   redireccionarHeroe(index: number):void{
-    this.router.navigate(['heroes/heroe', this.indexTarjeta]);
+   // this.router.navigate(['heroes/heroe', this.indexTarjeta]);
+   this.solicitarHeroe.emit(this.indexTarjeta);
   }
 }
